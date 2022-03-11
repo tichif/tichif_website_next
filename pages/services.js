d35eb977265ebfcb15dce97da392fc3a9b800b70
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-03-10 22:54:00 -0500

    Add translation to the services page

--- pages/services.js
+++ pages/services.js
@@ -1,15 +1,22 @@
+import { useRouter } from 'next/router';
+
 import Layout from '../components/layout';
 
 const Services = () => {
+  const router = useRouter();
   return (
     <Layout title='Services'>
       <section>
         <div className='title'>
-          <h1>The services I offer</h1>
+          <h1>
+            {router.locale === 'en-US'
+              ? 'The services I offer'
+              : "Les services que j'offre"}
+          </h1>
           <p>
-            I offer several services in the field of web development. Create
-            your website, increase your online presence or quickly become a
-            developer with a lower price.
+            {router.locale === 'en-US'
+              ? 'I offer several services in the field of web development. Create your website, increase your online presence or quickly become a developer with a lower price.'
+              : "J'offre plusieurs services dans le domaine du développement web. Créer votre site web, augment votre présence en ligne ou devenez rapidement un développeur grace aux prix imbattables sur le marché."}
           </p>
         </div>
         <div className='services'>
@@ -17,10 +24,14 @@ const Services = () => {
             <div className='icon'>
               <i className='fas fa-code'></i>
             </div>
-            <h2>Development</h2>
+            <h2>
+              {' '}
+              {router.locale === 'en-US' ? 'Development' : 'Développement'}
+            </h2>
             <p>
-              Create your website or web application using the latest
-              technologies such as React, Express, Docker, Kubernetes, Mongo DB.
+              {router.locale === 'en-US'
+                ? 'Create your website or web application using the latest technologies such as React, Express, Docker, Kubernetes, Mongo DB.'
+                : 'Création de votre site ou application web en utilisant les dernières technologies telles que React, Express, Docker, Kubernetes, Mongo DB.'}
             </p>
           </div>
 
@@ -28,10 +39,11 @@ const Services = () => {
             <div className='icon'>
               <i className='fas fa-server'></i>
             </div>
-            <h2>Hosting</h2>
+            <h2> {router.locale === 'en-US' ? 'Hosting' : 'Hébergement'}</h2>
             <p>
-              Deploy your website or web application on Digital Ocean, Heroku,
-              Netlify hosting platforms.
+              {router.locale === 'en-US'
+                ? 'Deploy your website or web application on Digital Ocean, Heroku, Netlify hosting platforms.'
+                : "Déploiement de votre site ou application web sur les plateformes d'hébergement Digital Ocean, Heroku, Netlify."}
             </p>
           </div>
 
@@ -39,10 +51,11 @@ const Services = () => {
             <div className='icon'>
               <i className='fas fa-chart-line'></i>
             </div>
-            <h2>SEO</h2>
+            <h2> {router.locale === 'en-US' ? 'SEO' : 'SEO'}</h2>
             <p>
-              Enhance your presence on the web thanks to search engine
-              optimization methods.
+              {router.locale === 'en-US'
+                ? 'Enhance your presence on the web thanks to search engine optimization methods.'
+                : "Renforcement de votre présence sur le web grace aux méthodes de référencement et d'optimisation."}
             </p>
           </div>
 
@@ -50,10 +63,14 @@ const Services = () => {
             <div className='icon'>
               <i className='fas fa-file-signature'></i>
             </div>
-            <h2>Consultation</h2>
+            <h2>
+              {' '}
+              {router.locale === 'en-US' ? 'Consultation' : 'Consultation'}
+            </h2>
             <p>
-              Opportunity study for the realization of website or web
-              application.
+              {router.locale === 'en-US'
+                ? 'Opportunity study for the realization of website or web application.'
+                : "Etude d'opportunité pour la réalisation de site ou d'application web."}
             </p>
           </div>
 
@@ -61,8 +78,13 @@ const Services = () => {
             <div className='icon'>
               <i className='fas fa-book'></i>
             </div>
-            <h2>Formation</h2>
-            <p>Online and face-to-face continuing education.</p>
+            <h2> {router.locale === 'en-US' ? 'Formation' : 'Formation'}</h2>
+            <p>
+              {' '}
+              {router.locale === 'en-US'
+                ? 'Online and face-to-face continuing education.'
+                : 'Formation continue en ligne et en présentielle.'}
+            </p>
           </div>
         </div>
       </section>
