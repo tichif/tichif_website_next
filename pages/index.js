6b59b51f07cb624fe5283bdfe4dd1d9979a888b0
Charles-Hebert Dalzon <charlby5@gmail.com>
2022-03-10 22:23:35 -0500

    Add translation in the homepage

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Image from 'next/image';
+import { useRouter } from 'next/router';
 
 import Layout from '../components/layout';
 
@@ -7,16 +7,24 @@ const clickHandler = () => {
 };
 
 const HomePage = () => {
+  const router = useRouter();
+
+  const description =
+    router.locale === 'en-US'
+      ? 'A web developer looking for the opportunity to bring his skills and apply them in a work environment that will encourage him to progress and acquire new knowledge, with the intention of helping the company achieve its goals.'
+      : "Un développeur web à la recherche de l'opportunité d'apporter ses compétences et de les appliquer dans un environnement de travail qui l'encouragera à progresser et à acquérir de nouvelles connaissances, dans l'intention d'aider l'entreprise à atteindre ses objectifs.";
+
   return (
-    <Layout title='Accueil'>
+    <Layout title={router.locale === 'en-US' ? 'Home' : 'Accueil'}>
       <section className='home'>
         <img src='/images/home.jpg' alt='tichif' className='home-image' />
         <div className='home-content'>
           <h3>Dalzon Charles-Hébert</h3>
           <small>Fullstack web developer</small>
-          <p>Description</p>
+          <p>{description}</p>
           <button className='btn' onClick={clickHandler}>
-            <i className='fas fa-download'></i> Download
+            <i className='fas fa-download'></i>{' '}
+            {router.locale === 'en-US' ? 'Download CV' : 'Télécharger CV'}
           </button>
         </div>
       </section>
